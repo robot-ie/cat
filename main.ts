68c51a81b2de7b8d4137fd0e70b4db94948e2e09
robot-ie <mig.ruiz+1@gmail.com>
2024-08-14 16:53:55 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,37 @@
 namespace cat {
-    //% block
-    //% group="Basic"
-    export function pressingLeftEar(): boolean {
+
+    let rightEarWasPressed = false
+    let numberOfTimeRightEarPressed = 0
+    basic.forever(function () {
+        const isPressingRightEarNow = checkIfPressingRightEar()
+        if (isPressingRightEarNow && !rightEarWasPressed){
+            numberOfTimeRightEarPressed = numberOfTimeRightEarPressed +1
+        }
+        rightEarWasPressed = isPressingRightEarNow
+        
+    })
+
+    function checkIfPressingRightEar(): boolean {
         const pin0Val = pins.analogReadPin(AnalogPin.P0)
         return pin0Val > 900;
     }
 
     //% block
     //% group="Basic"
-    export function onLeftEarPressed(handler: () => void) {
+    export function pressingRightEar(): boolean {
+        return rightEarWasPressed;
+    }
+
+    //% block
+    //% group="Basic"
+    export function onRightEarPressed(handler: () => void) {
+        let lastNumberOfTimesRightEarPressed = numberOfTimeRightEarPressed 
         basic.forever(function () {
-            const isPressingLeftEar = pressingLeftEar()
-            if (isPressingLeftEar){
-                handler();
+            if (numberOfTimeRightEarPressed>lastNumberOfTimesRightEarPressed){
+                handler()
             }
+            lastNumberOfTimesRightEarPressed = numberOfTimeRightEarPressed
         })
+        
     }
 }
