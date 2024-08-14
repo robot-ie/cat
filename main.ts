4f6f6b40ff4ae68ca1666f30f61785925d8b7cd7
robot-ie <mig.ruiz+1@gmail.com>
2024-08-14 16:30:56 +0100

    basic blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
+namespace cat {
+    //% group="Basic"
+    export function pressingLeftEar(): boolean {
+        const pin0Val = pins.analogReadPin(AnalogPin.P0)
+        return pin0Val > 900;
+    }
 
+    //% block
+    //% group="Basic"
+    export function onLeftEarPressed(handler: () => void) {
+        basic.forever(function () {
+            const isPressingLeftEar = pressingLeftEar()
+            if (isPressingLeftEar){
+                handler();
+            }
+        })
+    }
+}
